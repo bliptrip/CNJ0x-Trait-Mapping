34dab884caf11582edc3cfa95112a5a51c702484
Andrew Maule <maule2@wisc.edu>
2017-07-05 15:36:22 -0500

    Cleaned up shell file.

--- Scripts/Python/collate_all_accessions.sh
+++ Scripts/Python/collate_all_accessions.sh
@@ -4,17 +4,19 @@
 echo "This script is supposed to be run from the Script folder in order to function correctly with paths."
 
 DATA_FOLDER_PREFIX=$1
-output_template=$2
-output_file=$3
+RAW_DATA_PREFIX=$2
+DERIVED_DATA_PREFIX=$3
+output_template=$4
+output_file=$5
 
 #First copy the template the specified output file.
-output_template="${DATA_FOLDER_PREFIX}/Compiled/${output_template}"
-output_file="${DATA_FOLDER_PREFIX}/Compiled/${output_file}"
+output_template="${DATA_FOLDER_PREFIX}/${DERIVED_DATA_PREFIX}/${output_template}"
+output_file="${DATA_FOLDER_PREFIX}/${DERIVED_DATA_PREFIX}/${output_file}"
 
 cp -f "${output_template}" "${output_file}"
 
 while read file population year; do
-    cmd="./collate_separate_excel_datasets.py -i \"${DATA_FOLDER_PREFIX}/${file}\" --input_ws \"Sheet1\" -o \"${output_file}\" --output_ws \"Combined CNJ Upright Data\" -p $population -y $year"
+    cmd="./collate_separate_excel_datasets.py -i \"${DATA_FOLDER_PREFIX}/${RAW_DATA_PREFIX}/${file}\" --input_ws \"Sheet1\" -o \"${output_file}\" --output_ws \"Combined CNJ Upright Data\" -p $population -y $year"
     echo "$cmd"
     eval "$cmd"
 done < collate_datasets.txt
