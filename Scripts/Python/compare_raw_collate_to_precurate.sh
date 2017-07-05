4712e2de18de69af78099e0da2c9eeb14255d4f2
Andrew Maule <maule2@wisc.edu>
2017-07-05 15:29:26 -0500

    Adding additional parameters and adding ability to pass --convert_year to the command-line in order to convert between indexable years and gregorian calendar years.

--- Scripts/Python/compare_raw_collate_to_precurate.sh
+++ Scripts/Python/compare_raw_collate_to_precurate.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
 DATA_FOLDER_PREFIX=$1
-raw_collate=$2
-curate=$3
-output_template=$4
-output_file=$5
+DERIVED_FOLDER=$2
+raw_collate=$3
+curate=$4
+output_template=$5
+output_file=$6
+curate_row_start=$7
 
 #First copy the template the specified output file.
-raw_collate="${DATA_FOLDER_PREFIX}/Compiled/${raw_collate}"
-curate="${DATA_FOLDER_PREFIX}/Compiled/${curate}"
-output_template="${DATA_FOLDER_PREFIX}/Compiled/${output_template}"
-output_file="${DATA_FOLDER_PREFIX}/Compiled/${output_file}"
+raw_collate="${DATA_FOLDER_PREFIX}/${DERIVED_FOLDER}/${raw_collate}"
+curate="${DATA_FOLDER_PREFIX}/${DERIVED_FOLDER}/${curate}"
+output_template="${DATA_FOLDER_PREFIX}/${DERIVED_FOLDER}/${output_template}"
+output_file="${DATA_FOLDER_PREFIX}/${DERIVED_FOLDER}/${output_file}"
 
 cp -f "${output_template}" "${output_file}"
-cmd="./compare_raw_collate_to_precurate.py -r \"${raw_collate}\" --raw_ws \"Combined CNJ Upright Data\" -c \"${curate}\" --curated_ws \"Combined CNJ Upright Data\" -o \"${output_file}\" --output_ws \"Combined CNJ Upright Data\""
+if [ $# -eq 7 ]; then
+    cmd="./compare_raw_collate_to_precurate.py -r \"${raw_collate}\" --raw_ws \"Combined CNJ Upright Data\" -c \"${curate}\" --curated_ws \"Combined CNJ Upright Data\" -o \"${output_file}\" --output_ws \"Combined CNJ Upright Data\" --curate_row_start=$curate_row_start --convert_year"
+else
+    cmd="./compare_raw_collate_to_precurate.py -r \"${raw_collate}\" --raw_ws \"Combined CNJ Upright Data\" -c \"${curate}\" --curated_ws \"Combined CNJ Upright Data\" -o \"${output_file}\" --output_ws \"Combined CNJ Upright Data\" --curate_row_start=$curate_row_start"
+fi 
 echo $cmd
 eval $cmd 2> errors
