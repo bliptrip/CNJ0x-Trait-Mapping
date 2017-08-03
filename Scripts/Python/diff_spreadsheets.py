55e58560bf4e14ccb5902b5066ba2a40aae0adf6
Andrew Maule <maule2@wisc.edu>
2017-08-03 14:58:08 -0500

    Adding full printout of sheet, not just errrors, along with estimated error counts/percent errors.

--- Scripts/Python/diff_spreadsheets.py
+++ Scripts/Python/diff_spreadsheets.py
@@ -2,8 +2,11 @@
 
 import sys
 import argparse
+import re
 from openpyxl import load_workbook,Workbook
 from openpyxl.styles import PatternFill,fills,colors
+
+RE_ROWCOL_HEADER = r'Row/Col' 
 
 def parse_args():
     parser = argparse.ArgumentParser(prog=sys.argv[0], description="Does a raw diff of two spreadsheets, outputting the differences to a file with differences highlighted in red.")
@@ -21,6 +24,10 @@ def compare_entries(raw_cell, curated_cell):
     if(raw_cell.value != curated_cell.value):
         string = "RAW: \"%s\", CURATED: \"%s\""%(str(raw_cell.value), str(curated_cell.value))
     return string
+
+def copy_cells(ws_output, output_row, ws_source_cells):
+    for i,cell in enumerate(ws_source_cells):
+        ws_output.cell(column=i+1, row=output_row, value=cell.value)
 
 def copy_differences(ws_file, file_row, ws_output, output_row, cell_strings, fill):
     for i,cell_string in enumerate(cell_strings):
@@ -43,17 +50,37 @@ if __name__ == '__main__':
     ws_output        = wb_output.active
 
     difference_fill        = PatternFill(fgColor=colors.RED,bgColor=colors.RED,fill_type=fills.FILL_SOLID)
+    re_header_compile      = re.compile(RE_ROWCOL_HEADER, re.I)
     ws_output_current_row  = 1
+    total_errors_count     = 0
+    total_genotypes        = 0
+
+    
     #Setup the map of the row/col accession and defined year to an index year
     #initialize the RC_ACCESSION_TO_YEAR dictionary to []
     max_row = max(ws_file_1.max_row,ws_file_2.max_row)
     for row in range(1,max_row+1):
         file_1_cells   = ws_file_1['A'+str(row)+':V'+str(row)][0]
         file_2_cells   = ws_file_2['A'+str(row)+':V'+str(row)][0]
+        if re_header_compile.match(str(file_2_cells[0].value)):
+            total_genotypes += 1
         expected_column_count = len(file_1_cells)
         strings = map(compare_entries, file_1_cells, file_2_cells)
+        total_errors_count += (len(strings) - strings.count(None))
         if( strings.count(None) < expected_column_count ):
             copy_differences(ws_file_1, row, ws_output, ws_output_current_row, strings, difference_fill)
             ws_output_current_row += 1
+        else:
+            copy_cells(ws_output, ws_output_current_row, file_2_cells)
+            ws_output_current_row += 1
+
+    error_count_row = 2
+    ws_output.cell(column=1, row=error_count_row, value="No. Error Phenotype").fill = difference_fill
+    ws_output.cell(column=2, row=error_count_row, value="%d"%(total_errors_count)).fill = difference_fill
+    approximate_num_phenotypes = total_genotypes * 10 * 21 #approximately 10 uprights per genotype, 21 phenotypic variables measured per upright
+    ws_output.cell(column=3, row=error_count_row, value="Approx No. Total Phenotypes").fill = difference_fill
+    ws_output.cell(column=4, row=error_count_row, value="%d"%(approximate_num_phenotypes)).fill = difference_fill
+    ws_output.cell(column=5, row=error_count_row, value="Percent Error").fill = difference_fill
+    ws_output.cell(column=6, row=error_count_row, value="%.2f"%((total_errors_count*100.0)/approximate_num_phenotypes)).fill = difference_fill
 
     wb_output.save(parsed.output)
