bd74fa297dfd29a886b05a0e49c758dd44f9f0df
Andrew Maule <maule2@wisc.edu>
2018-01-10 14:30:29 -0600

    Updating to read an rds cross object that has had calc.genoprob() executed on it.  Also, upgraded to use the loopThruTraits() command.

--- Scripts/R/qtl_pipeline_04_scanone.R
+++ Scripts/R/qtl_pipeline_04_scanone.R
@@ -19,21 +19,17 @@ if(length(args)==0) {
 }
 
 source(paste0(workflow,"/configs/model.cfg"))
+source('./usefulFunctions.R')
 
 traits.df <- read.csv(file=paste0(workflow,"/configs/model-traits.cfg.csv"),header=T, stringsAsFactors=F)
-for( i in 1:length(traits.df[,1]) ) {
-    trait.cfg       <- traits.df[i,]
-    if ( is.na(trait.cfg$mask) || (trait.cfg$mask != "TRUE") ) {
-        traits           <- unlist(strsplit(trait.cfg$mtraits,","))
-        trait.names      <- paste0(traits,collapse="__")
-        trait_subfolder  <- paste0(c(trait.cfg$model,trait.names),collapse="--")
-        trait_subfolder_fpath <- file.path(paste0(workflow,"/traits"), trait_subfolder)
-        cross <- read.cross(format='csv', file=paste0(trait_subfolder_fpath,"/cross.csv"), genotypes=NULL)
-        cross <- calc.genoprob(cross, step=0, map.function="kosambi")
-        scan.one  <- scanone(cross, pheno.col=traits, method=qtl_method, verbose=FALSE)
-        scan.one.perms <- readRDS(paste0(trait_subfolder_fpath, "/operms.rds"))
-        scan.one.summary <- summary(scan.one, perms=scan.one.perms, alpha=0.2, pvalues=TRUE)
-        saveRDS(scan.one, file=paste0(trait_subfolder_fpath, "/scanone.rds"), compress=TRUE)
-        saveRDS(scan.one.summary, file=paste0(trait_subfolder_fpath, "/scanone.summary.rds"), compress=TRUE)
-    }
+
+merge1D <- function(trait.cfg, trait.names, traits, trait.path, funArgs) {
+    cross            <- readRDS(file=paste0(trait.path,"/cross.rds"))
+    scan.one         <- scanone(cross, pheno.col=traits, method=qtl_method, verbose=FALSE)
+    scan.one.perms   <- readRDS(paste0(trait.path, "/operms.rds"))
+    scan.one.summary <- summary(scan.one, perms=scan.one.perms, alpha=0.2, pvalues=TRUE)
+    saveRDS(scan.one, file=paste0(trait.path, "/scanone.rds"), compress=TRUE)
+    saveRDS(scan.one.summary, file=paste0(trait.path, "/scanone.summary.rds"), compress=TRUE)
 }
+
+loopThruTraits(workflow, merge1D)
