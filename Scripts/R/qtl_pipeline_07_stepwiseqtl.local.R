1974f2353ac83033432a501039d302d8891348bb
Andrew Maule <maule2@wisc.edu>
2018-01-10 14:41:00 -0600

    Updating to read an rds cross object that has had calc.genoprob() executed on it.

--- Scripts/R/qtl_pipeline_07_stepwiseqtl.local.R
+++ Scripts/R/qtl_pipeline_07_stepwiseqtl.local.R
@@ -17,27 +17,22 @@ if(length(args)==0) {
 }
 
 source(paste0(workflow,"/configs/model.cfg"))
+source('./usefulFunctions.R')
 
 #Loop over all mmers, trait groups, subgroups, and perform stepwiseqtl()
-traits.df <- read.csv(file=paste0(workflow,"/configs/model-traits.cfg.csv"),header=T,stringsAsFactors=F)
-for( i in 1:length(traits.df[,1]) ) {
-    trait.cfg       <- traits.df[i,]
-    if ( is.na(trait.cfg$mask) || (trait.cfg$mask != "TRUE") ) {
-        traits           <- unlist(strsplit(trait.cfg$mtraits,","))
-        trait.names      <- paste0(traits,collapse="__")
-        trait_subfolder  <- paste0(c(trait.cfg$model,trait.names),collapse="--")
-        trait_subfolder_fpath <- file.path(paste0(workflow,"/traits"), trait_subfolder)
-        cross <- read.cross(format='csv', file=paste0(trait_subfolder_fpath,"/cross.csv"), genotypes=NULL)
-        cross <- calc.genoprob(cross, step=0, map.function="kosambi")
-        scan.two.perms <- readRDS(paste0(trait_subfolder_fpath, "/operms.2D.rds"))
-        pens  <- calc.penalties(scan.two.perms)
-        for( j in 1:length(traits) ) {
-            trait <- traits[j]
-            print(paste0("Running stepwiseqtl() with model: ",trait.cfg$model," | traits: ",trait.names, " | trait: ", trait))
-            trait_subsubfolder_fpath = paste0(trait_subfolder_fpath, '/', trait) 
-            dir.create(trait_subsubfolder_fpath, showWarnings = FALSE)
-            scan.sw <- stepwiseqtl(cross,pheno.col=trait,max.qtl=qtl_max,method=qtl_method,penalties=pens[j,],additive.only=FALSE)
-            saveRDS(scan.sw, file=paste0(trait_subsubfolder_fpath, "/scansw.rds"), compress=TRUE)
-        }
+stepwiseqtlAll <- function(trait.cfg, trait.names, traits, trait.path, funArgs) {
+    cross <- readRDS(file=paste0(trait.path,"/cross.rds"))
+    scan.two.perms <- readRDS(paste0(trait.path, "/operms.2D.rds"))
+    pens  <- calc.penalties(scan.two.perms)
+    for( j in 1:length(traits) ) {
+        trait <- traits[j]
+        print(paste0("Running stepwiseqtl() with model: ",trait.cfg$model," | traits: ",trait.names, " | trait: ", trait))
+        trait_subsubfolder_fpath = paste0(trait.path, '/', trait) 
+        dir.create(trait_subsubfolder_fpath, showWarnings = FALSE)
+        scan.sw <- stepwiseqtl(cross,pheno.col=trait,max.qtl=qtl_max,method=qtl_method,penalties=pens[j,],additive.only=FALSE)
+        saveRDS(scan.sw, file=paste0(trait_subsubfolder_fpath, "/scansw.rds"), compress=TRUE)
     }
 }
+
+#Loop through all legitimate traits and build collated qtl file.
+loopThruTraits(workflow, stepwiseqtlAll)
