37d7165cec34ee28e3aab63d16f15744011e8ab7
Andrew Maule <maule2@wisc.edu>
2018-01-10 14:53:52 -0600

    Adding append.pointer() to be able to add data to a 'pointer' in order to build up the collated qtl list.

--- Scripts/R/qtl_pipeline_09_collate.R
+++ Scripts/R/qtl_pipeline_09_collate.R
@@ -27,29 +27,31 @@ supermap.bin.df <- readRDS(file=geno_rpath2fpath(paste0(geno_consensus_file,".rd
 #index into qtl.collated.df
 qtl.collated.df <- data.frame(model=character(),year=numeric(),mtraits=character(),trait=character(),chr=numeric(),position=numeric(),nearest.marker=numeric(),marker.variance=numeric(),model.variance=numeric(),interval=numeric(),stringsAsFactors=FALSE)
 qtl.collated.df.p <- newPointer(qtl.collated.df)
-collate_qtl     <- function(trait.cfg, trait.names, trait, trait.path, funArgs) {
-		model <- as.character(trait.cfg$model)
-		year  <- as.numeric(trait.cfg$year)
-        trait_subsubfolder_fpath <- file.path(trait.path, trait)
-        scan.sw.mdres  <- readRDS(file=paste0(trait_subsubfolder_fpath,'/scansw.mdres.rds'))
-        qtl.num  <- length(scan.sw.mdres$model$chr)
-        if( qtl.num > 0 ) {
-            for (i in 1:qtl.num) {
-                mypos     <- scan.sw.mdres$model$pos[i]
-                mychr     <- as.numeric(scan.sw.mdres$model$chr[i])
-                f         <- which(supermap.bin.df$LG==mychr)
-                x         <- supermap.bin.df[f,]
-                idx       <- which.min(abs(x$consensus-mypos))
-                mymarker  <- paste0(x$binID[idx],'_',x$marker[idx])
-                f2        <- intersect(scan.sw.mdres$model$name[i],rownames(scan.sw.mdres$drop))
-                if (qtl.num==1) {
-                    x.var <- scan.sw.mdres$variance
-                } else {
-                    x2    <- scan.sw.mdres$drop[f2,]
-                    x.var <- x2$'%var'
+collate_qtl     <- function(trait.cfg, trait.names, traits, trait.path, funArgs) {
+        model <- as.character(trait.cfg$model)
+        year  <- as.numeric(trait.cfg$year)
+        for( trait in traits ) {
+                trait_subsubfolder_fpath <- file.path(trait.path, trait)
+                scan.sw.mdres  <- readRDS(file=paste0(trait_subsubfolder_fpath,'/scansw.mdres.rds'))
+                qtl.num  <- length(scan.sw.mdres$model$chr)
+                if( qtl.num > 0 ) {
+                        for (i in 1:qtl.num) {
+                                mypos     <- scan.sw.mdres$model$pos[i]
+                                mychr     <- as.numeric(scan.sw.mdres$model$chr[i])
+                                f         <- which(supermap.bin.df$LG==mychr)
+                                x         <- supermap.bin.df[f,]
+                                idx       <- which.min(abs(x$consensus-mypos))
+                                mymarker  <- paste0(x$binID[idx],'_',x$marker[idx])
+                                f2        <- intersect(scan.sw.mdres$model$name[i],rownames(scan.sw.mdres$drop))
+                                if (qtl.num==1) {
+                                        x.var <- scan.sw.mdres$variance
+                                } else {
+                                        x2    <- scan.sw.mdres$drop[f2,]
+                                        x.var <- x2$'%var'
+                                }
+                                append.pointer(funArgs, c(model, year, trait.names, trait, mychr, mypos, mymarker, x.var, scan.sw.mdres$variance, scan.sw.mdres$inter[i]))
+                        }
                 }
-                append.pointer(funArgs, c(model, year, trait.names, trait, mychr, mypos, mymarker, x.var, scan.sw.mdres$variance, scan.sw.mdres$inter[i]))
-            }
         }
 }
 
