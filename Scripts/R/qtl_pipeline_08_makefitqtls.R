3db789e998efcf026341f3815cfab37637d52d47
Andrew Maule <maule2@wisc.edu>
2018-02-13 14:49:42 -0600

    Reformatting to use the loopThruTraits() wrapper and callbacks.

--- Scripts/R/qtl_pipeline_08_makefitqtls.R
+++ Scripts/R/qtl_pipeline_08_makefitqtls.R
@@ -16,37 +16,31 @@ if(length(args)==0) {
 
 source(paste0(workflow,"/configs/model.cfg"))
 
-#Loop over all mmers, trait groups, subgroups, and perform makeqtl() and fitqtl().
 traits.df <- read.csv(file=paste0(workflow,"/configs/model-traits.cfg.csv"),header=T,stringsAsFactors=F)
-for( i in 1:length(traits.df[,1]) ) {
-    trait.cfg       <- traits.df[i,]
-    if ( is.na(trait.cfg$mask) || (trait.cfg$mask != "TRUE") ) {
-        traits           <- unlist(strsplit(trait.cfg$mtraits,","))
-        trait.names      <- paste0(traits,collapse="__")
-        trait_subfolder  <- paste0(c(trait.cfg$model,trait.names),collapse="--")
-        trait_subfolder_fpath <- file.path(paste0(workflow,"/traits"), trait_subfolder)
-        cross <- read.cross(format='csv', file=paste0(trait_subfolder_fpath,"/cross.csv"), genotypes=NULL)
-        cross <- calc.genoprob(cross, step=0, map.function="kosambi")
-        scan.two.perms <- readRDS(paste0(trait_subfolder_fpath, "/operms.2D.rds"))
-        for( j in 1:length(traits) ) {
-            trait <- traits[j]
-            trait_subsubfolder_fpath <- file.path(trait_subfolder_fpath, trait)
-            scan.sw <- readRDS(paste0(trait_subsubfolder_fpath, "/scansw.rds"))
-            print(paste0("Running makeqtl() and fitqtl() with model: ",trait.cfg$model," | traits: ",trait.names, " | trait: ", trait))
-            scan.sw.qtl <- makeqtl(cross, chr=scan.sw$chr, pos=scan.sw$pos, what='prob')
-            scan.sw.md  <- fitqtl(cross, pheno.col=c(trait), scan.sw.qtl, formula = formula(scan.sw), get.ests=F)
-            scan.sw.summary <-summary(scan.sw.md)
-            scan.sw.mdres <-list(variance=scan.sw.summary$result.full[1,5], drop=as.data.frame(scan.sw.summary$result.drop), model=scan.sw, inter=numeric())
-            for (k in 1:length(scan.sw$chr)){
-                mylod<-diff(lodint(scan.sw,qtl.index=k)[c(1,3),2])
-                scan.sw.mdres$inter[k]<-mylod
-            }
-            saveRDS(scan.sw.qtl, file=paste0(trait_subsubfolder_fpath,'/scansw.qtl.rds'),compress=T)
-            saveRDS(scan.sw.md, file=paste0(trait_subsubfolder_fpath,'/scansw.md.rds'),compress=T)
-            saveRDS(scan.sw.summary, file=paste0(trait_subsubfolder_fpath,'/scansw.summary.rds'),compress=T)
-            saveRDS(scan.sw.mdres , file=paste0(trait_subsubfolder_fpath,'/scansw.mdres.rds'),compress=T)
+
+#Loop over all mmers, trait groups, subgroups, and perform makeqtl() and fitqtl().
+fitQtlCB <- function(trait.cfg, trait.names, traits, trait.path, funArgs) {
+    cross <- readRDS(file=paste0(trait.path,"/cross.rds"))
+    scan.two.perms <- readRDS(paste0(trait.path, "/operms.2D.rds"))
+    for( j in 1:length(traits) ) {
+        trait <- traits[j]
+        trait_subsubfolder_fpath <- file.path(trait.path, trait);
+        scan.sw <- readRDS(paste0(trait_subsubfolder_fpath, "/scansw.rds"))
+        print(paste0("Running makeqtl() and fitqtl() with model: ",trait.cfg$model," | traits: ",trait.names, " | trait: ", trait))
+        scan.sw.qtl <- makeqtl(cross, chr=scan.sw$chr, pos=scan.sw$pos, what='prob')
+        scan.sw.md  <- fitqtl(cross, pheno.col=c(trait), scan.sw.qtl, formula = formula(scan.sw), get.ests=F)
+        scan.sw.summary <-summary(scan.sw.md)
+        scan.sw.mdres <-list(variance=scan.sw.summary$result.full[1,5], drop=as.data.frame(scan.sw.summary$result.drop), model=scan.sw, inter=numeric())
+        for (k in 1:length(scan.sw$chr)){
+            mylod<-diff(lodint(scan.sw,qtl.index=k)[c(1,3),2])
+            scan.sw.mdres$inter[k]<-mylod
         }
+        saveRDS(scan.sw.qtl, file=paste0(trait_subsubfolder_fpath,'/scansw.qtl.rds'),compress=T)
+        saveRDS(scan.sw.md, file=paste0(trait_subsubfolder_fpath,'/scansw.md.rds'),compress=T)
+        saveRDS(scan.sw.summary, file=paste0(trait_subsubfolder_fpath,'/scansw.summary.rds'),compress=T)
+        saveRDS(scan.sw.mdres , file=paste0(trait_subsubfolder_fpath,'/scansw.mdres.rds'),compress=T)
     }
 }
 
-
+#Loop through all legitimate traits and build collated qtl file.
+loopThruTraits(workflow, fitQtlCB)
