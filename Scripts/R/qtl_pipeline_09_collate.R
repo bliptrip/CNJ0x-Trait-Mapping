034f2ba07367b3015f01afa682d7447f45499742
Andrew Maule <maule2@wisc.edu>
2018-02-20 17:16:00 -0600

    Major overhaul.  1) Incorporated scanone() results into qtl table, with a new column indicating the source of the qtl data. 2) Incorporated pairwise interaction qtls, if they exist, adding columns position2 and chr2 to show the links.  3) Added a qtl.lod column to store the additive and/or pairwise interaction qtls. 4) Added qtl.pvalue in case this information is important for visiting/visualizing.

--- Scripts/R/qtl_pipeline_09_collate.R
+++ Scripts/R/qtl_pipeline_09_collate.R
@@ -23,36 +23,87 @@ source(paste0(workflow,"/configs/model.cfg"))
 #Read in the consensus map file, modified in qtl_pipeline_01_genmodels.R
 supermap.bin.df <- readRDS(file=geno_rpath2fpath(paste0(geno_consensus_file,".rds")))
 
+#Function to deconstruct a QTL formula into it's parts
+deconstruct_qtl_formula <- function(qtl_formula) {
+    qtls <- trimws(unlist(strsplit(gsub("y[ ]*~[ ]*(.+)","\\1",qtl_formula),"\\+")))
+    return(qtls)
+}
+
+#Function that iterates through a set of model qtls and fills in the collated datatable
+generate_qtl_collate <- function(qtl, qtl_model, method, model, year, trait.names, trait, trait_subfolder_path, collated.df.p) {
+    qtl.model.terms  <- deconstruct_qtl_formula(formula(qtl))
+    qtl.num          <- length(qtl.model.terms)
+    for (qtl.model.term in qtl.model.terms) {
+        #Initialize some of the entries to NA for each step.
+        mychr2        <- NA
+        mypos2        <- NA
+        mymarker      <- NA
+        qtl_lodint    <- NA
+
+        #Is this an QTL:QTL pairwise interaction or just a single additive QTL effect
+        qtl.model.term.v   <- unlist(strsplit(qtl.model.term,":"))
+        qtl_idx1           <- which(qtl$altname == qtl.model.term.v[1])
+        mychr              <- as.numeric(qtl$chr[qtl_idx1])
+        mypos              <- qtl$pos[qtl_idx1]
+        #single-additive effect
+        if( length(qtl.model.term.v) == 1 ) {
+            qtl_lodint    <- diff(lodint(qtl,qtl.index=qtl_idx1)[c(1,3),2])
+            f             <- which(supermap.bin.df$LG==mychr)
+            x             <- supermap.bin.df[f,]
+            idx           <- which.min(abs(x$consensus-mypos))
+            mymarker      <- paste0(x$binID[idx],'_',x$marker[idx])
+            f2            <- intersect(qtl$name[qtl_idx1],rownames(qtl_model$result.drop))
+            qtl_model.var <- qtl_model$result.full["Model","%var"]
+            if (qtl.num==1) {
+                qtl.var   <- qtl_model$result.full["Model","%var"]
+                qtl.lod   <- qtl_model$result.full["Model","LOD"]
+                qtl.p     <- qtl_model$result.full["Model","Pvalue(F)"]
+            } else {
+                qtl.var   <- qtl_model$result.drop[f2,"%var"]
+                qtl.lod   <- qtl_model$result.drop[f2,"LOD"]
+                qtl.p     <- qtl_model$result.drop[f2,"Pvalue(F)"]
+            }
+        } else { #pairwise effect
+            qtl_idx2      <- which(qtl$altname == qtl.model.term.v[2])
+            mychr2        <- as.numeric(qtl$chr[qtl_idx2])
+            mypos2        <- qtl$pos[qtl_idx2]
+            qtl_model.var <- qtl_model$result.full["Model","%var"]
+            qtl_name      <- paste0(qtl$name[qtl_idx1],":",qtl$name[qtl_idx2]);
+            f2            <- intersect(qtl_name,rownames(qtl_model$result.drop))
+            if (qtl.num==1) {
+                qtl.var   <- qtl_model$result.full["Model","%var"]
+                qtl.lod   <- qtl_model$result.full["Model","LOD"]
+                qtl.p     <- qtl_model$result.full["Model","Pvalue(F)"]
+            } else {
+                qtl.var   <- qtl_model$result.drop[f2,"%var"]
+                qtl.lod   <- qtl_model$result.drop[f2,"LOD"]
+                qtl.p     <- qtl_model$result.drop[f2,"Pvalue(F)"]
+            }
+        }
+        #TODO: Fill in interaction qtls
+        append.pointer(collated.df.p, c(method, model, year, trait.names, trait, mychr, mypos, mychr2, mypos2, mymarker, qtl.lod, qtl.var, qtl.p, qtl_model.var, qtl_lodint))
+    }
+}
+
+
 #Generate a single dataframe with qtl information in it, and save to a csv file.
 #index into qtl.collated.df
-qtl.collated.df <- data.frame(model=character(),year=numeric(),mtraits=character(),trait=character(),chr=numeric(),position=numeric(),nearest.marker=numeric(),marker.variance=numeric(),model.variance=numeric(),interval=numeric(),stringsAsFactors=FALSE)
+qtl.collated.df <- data.frame(method=character(),model=character(),year=numeric(),mtraits=character(),trait=character(),chr=numeric(),position=numeric(),chr2=numeric(),position2=numeric(),nearest.marker=numeric(),qtl.lod=numeric(),marker.variance=numeric(),qtl.pvalue=numeric(),model.variance=numeric(),interval=numeric(),stringsAsFactors=FALSE)
 qtl.collated.df.p <- newPointer(qtl.collated.df)
 collateQtlCB     <- function(trait.cfg, trait.names, traits, trait.path, funArgs) {
-        model <- as.character(trait.cfg$model)
-        year  <- as.numeric(trait.cfg$year)
-        for( trait in traits ) {
-                trait_subsubfolder_fpath <- file.path(trait.path, trait)
-                scan.sw.mdres  <- readRDS(file=paste0(trait_subsubfolder_fpath,'/scansw.mdres.rds'))
-                qtl.num  <- length(scan.sw.mdres$model$chr)
-                if( qtl.num > 0 ) {
-                        for (i in 1:qtl.num) {
-                                mypos     <- scan.sw.mdres$model$pos[i]
-                                mychr     <- as.numeric(scan.sw.mdres$model$chr[i])
-                                f         <- which(supermap.bin.df$LG==mychr)
-                                x         <- supermap.bin.df[f,]
-                                idx       <- which.min(abs(x$consensus-mypos))
-                                mymarker  <- paste0(x$binID[idx],'_',x$marker[idx])
-                                f2        <- intersect(scan.sw.mdres$model$name[i],rownames(scan.sw.mdres$drop))
-                                if (qtl.num==1) {
-                                        x.var <- scan.sw.mdres$variance
-                                } else {
-                                        x2    <- scan.sw.mdres$drop[f2,]
-                                        x.var <- x2$'%var'
-                                }
-                                append.pointer(funArgs, c(model, year, trait.names, trait, mychr, mypos, mymarker, x.var, scan.sw.mdres$variance, scan.sw.mdres$inter[i]))
-                        }
-                }
-        }
+    model <- as.character(trait.cfg$model)
+    year  <- as.numeric(trait.cfg$year)
+    for( trait in traits ) {
+        trait_subsubfolder_fpath <- file.path(trait.path, trait)
+        #scanone-derived results
+        scan.one.qtl <- readRDS(file=paste0(trait_subsubfolder_fpath,'/scanone.qtl.rds'))
+        scan.one.md  <- readRDS(file=paste0(trait_subsubfolder_fpath,'/scanone.md.rds'))
+        generate_qtl_collate(scan.one.qtl, scan.one.md, "scanone", model, year, trait.names, trait, trait_subsubfolder_fpath, funArgs)
+        #stepwiseqtl-derived results
+        scan.sw.qtl <- readRDS(file=paste0(trait_subsubfolder_fpath,'/scansw.rds'))
+        scan.sw.md  <- readRDS(file=paste0(trait_subsubfolder_fpath,'/scansw.md.rds'))
+        generate_qtl_collate(scan.sw.qtl, scan.sw.md, "stepwiseqtl", model, year, trait.names, trait, trait_subsubfolder_fpath, funArgs)
+    }
 }
 
 #Loop through all legitimate traits and build collated qtl file.
