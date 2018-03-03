4b5b244dd789082b114cb4e79609562ce6256e21
Andrew Maule <maule2@wisc.edu>
2018-03-03 14:23:33 -0600

    Changed code that calculated means and separated populations to use dplyr-like constructs.  Also, added a few calls to generate gplot2-like correlation matrices, etc.

--- Scripts/R/qtl_pipeline_00_1_means.R
+++ Scripts/R/qtl_pipeline_00_1_means.R
@@ -7,6 +7,8 @@
 # loading libraries
 source('./usefulFunctions.R')
 
+library(dplyr)
+
 #Read the phenotypic data in that was output by analyze_remove_outliers.R
 
 cnjpop.pheno.df <- readRDSw('cnjpop.noout.df.rds')
@@ -22,6 +24,14 @@ if(length(args)!=0) {
     }
 }
 
+extract_row <- function(accession) {
+    return(gsub("R([[:digit:]]+)C([[:digit:]]+)","\\1",accession,ignore.case=T))
+}
+
+extract_col <- function(accession) {
+    return(gsub("R([[:digit:]]+)C([[:digit:]]+)","\\2",accession,ignore.case=T))
+}
+
 #First thing first, only consider progeny
 cnjpop.pheno.progeny.idx <- which(grepl("CNJ0.*", cnjpop.pheno.df$accession_name))
 cnjpop.pheno.df <- cnjpop.pheno.df[cnjpop.pheno.progeny.idx,]
@@ -34,38 +44,36 @@ cnjpop.pheno.df$berry_weight[cnjpop.pheno.df.length_width_weight.zeros.idx] <- N
 cnjpop.pheno.df[cnjpop.pheno.df.length_width_weight.zeros.idx,]
 cnjpop.pheno.df.num_seeds.zeros.idx <- which(cnjpop.pheno.df$num_seeds == 0)
 cnjpop.pheno.df$num_seeds[cnjpop.pheno.df.num_seeds.zeros.idx] <- NA
+#Add row/column designators to indicate the location                            
+cnjpop.pheno.df <- cnjpop.pheno.df %>%
+                      mutate(row=extract_row(accession),
+                             column=extract_col(accession))
 
-#Compute means for each genotype x year across uprights
-cnjpop.pheno.berry_length.means.df <- aggregate(formula=berry_length~population+year+accession_name,data=cnjpop.pheno.df,FUN=mean,na.omit=T)
-cnjpop.pheno.berry_width.means.df <- aggregate(formula=berry_width~population+year+accession_name,data=cnjpop.pheno.df,FUN=mean,na.omit=T)
-cnjpop.pheno.berry_weight.means.df <- aggregate(formula=berry_weight~population+year+accession_name,data=cnjpop.pheno.df,FUN=mean,na.omit=T)
-cnjpop.pheno.num_seeds.means.df <- aggregate(formula=num_seeds~population+year+accession_name,data=cnjpop.pheno.df,FUN=mean,na.omit=T)
-cnjpop.pheno.num_peds.means.df <- aggregate(formula=num_peds~population+year+accession_name,data=cnjpop.pheno.df,FUN=mean,na.omit=T)
-cnjpop.pheno.num_berries.means.df <- aggregate(formula=num_berries~population+year+accession_name,data=cnjpop.pheno.df,FUN=mean,na.omit=T)
-cnjpop.pheno.total_berry_weight.means.df <- aggregate(formula=total_berry_weight~population+year+accession_name,data=cnjpop.pheno.df,FUN=mean,na.omit=T)
+#Calculate means of uprights
+cnjpop.pheno.means.df <- cnjpop.pheno.df %>%
+                            group_by(population,year,accession_name, accession) %>%
+                            summarize(berry_length=mean(berry_length, na.rm=T), 
+                                      berry_width=mean(berry_width, na.rm=T),
+                                      berry_weight=mean(berry_weight, na.rm=T),
+                                      num_seeds=mean(num_seeds, na.rm=T),
+                                      num_peds=mean(num_peds, na.rm=T),
+                                      num_berries=mean(num_berries, na.rm=T),
+                                      total_berry_weight=mean(total_berry_weight, na.rm=T))
+#Add row/column designators to indicate the location                            
+cnjpop.pheno.means.df <- cnjpop.pheno.means.df %>%
+                            mutate(row=extract_row(accession),
+                                   column=extract_col(accession))
 
-
-#Combine means into one dataframe
-#cnjpop.pheno.means.df <- cbind(cnjpop.pheno.berry_length.means.df, berry_width=cnjpop.pheno.berry_width.means.df[,4], berry_weight=cnjpop.pheno.berry_weight.means.df[,4], num_seeds=cnjpop.pheno.num_seeds.means.df[,4], num_peds=cnjpop.pheno.num_peds.means.df[,4], num_berries=cnjpop.pheno.num_berries.means.df[,4], total_berry_weight=cnjpop.pheno.total_berry_weight.means.df[,4])
-cnjpop.pheno.merge.df <- merge(cnjpop.pheno.berry_length.means.df, cnjpop.pheno.berry_width.means.df, by=c("population", "year", "accession_name"))
-cnjpop.pheno.merge.df <- merge(cnjpop.pheno.merge.df, cnjpop.pheno.berry_weight.means.df, by=c("population", "year", "accession_name"))
-cnjpop.pheno.merge.df <- merge(cnjpop.pheno.merge.df, cnjpop.pheno.num_seeds.means.df,by=c("population", "year", "accession_name"))
-cnjpop.pheno.merge.df <- merge(cnjpop.pheno.merge.df, cnjpop.pheno.num_peds.means.df,by=c("population", "year", "accession_name"))
-cnjpop.pheno.merge.df <- merge(cnjpop.pheno.merge.df, cnjpop.pheno.num_berries.means.df,by=c("population", "year", "accession_name"))
-cnjpop.pheno.means.df <- merge(cnjpop.pheno.merge.df, cnjpop.pheno.total_berry_weight.means.df, by=c("population", "year", "accession_name"))
-#colnames(cnjpop.pheno.means.df) <- c("population", "year", "accession_name", "berry_length", "berry_width", "berry_weight", "num_seeds", "num_peds", "num_berries", "total_berry_weight")
 
 #Separate two populations for analysis
-#Means
-cnjpop.pheno.p1.means.df.idx <- which(cnjpop.pheno.means.df$population=="1")
-cnjpop.pheno.p1.means.df <- cnjpop.pheno.means.df[cnjpop.pheno.p1.means.df.idx,-1] #remove the population field as no longer needed
-cnjpop.pheno.p2.means.df.idx <- which(cnjpop.pheno.means.df$population=="2")
-cnjpop.pheno.p2.means.df <- cnjpop.pheno.means.df[cnjpop.pheno.p2.means.df.idx,-1] #Remove the population field as no longer needed
-#All values
-cnjpop.pheno.p1.df.idx <- which(cnjpop.pheno.df$population=="1")
-cnjpop.pheno.p1.df <- cnjpop.pheno.df[cnjpop.pheno.p1.df.idx,-1] #Remove the population field as no longer needed
-cnjpop.pheno.p2.df.idx <- which(cnjpop.pheno.df$population=="2")
-cnjpop.pheno.p2.df <- cnjpop.pheno.df[cnjpop.pheno.p2.df.idx,-1] #Remove the population field as no longer needed
+cnjpop.pheno.p1.means.df <- cnjpop.pheno.means.df %>%
+                                filter(population == 1)
+#Remove the population column
+cnjpop.pheno.p1.means.df <- subset(cnjpop.pheno.p1.means.df, select=-c(population))                                
+cnjpop.pheno.p2.means.df <- cnjpop.pheno.means.df %>%
+                                filter(population == 2)
+#Remove the population column
+cnjpop.pheno.p2.means.df <- subset(cnjpop.pheno.p2.means.df, select=-c(population))                                
 
 ############### NOTE: Maybe consider removing the 'complete gentoype' code below when considering across each year itself. #####################
 
@@ -74,13 +82,11 @@ cnjpop.pheno.p2.df <- cnjpop.pheno.df[cnjpop.pheno.p2.df.idx,-1] #Remove the pop
 cnjpop.pheno.p1.means.table <- table(cnjpop.pheno.p1.means.df$accession_name) 
 #Complete genotypes should apply for both full and means data
 cnjpop.pheno.p1.CompleteGenoTypes <- names(which(cnjpop.pheno.p1.means.table == 3))
-cnjpop.pheno.p1.df <- cnjpop.pheno.p1.df[cnjpop.pheno.p1.df$accession_name %in% cnjpop.pheno.p1.CompleteGenoTypes,]
 cnjpop.pheno.p1.means.df <- cnjpop.pheno.p1.means.df[cnjpop.pheno.p1.means.df$accession_name %in% cnjpop.pheno.p1.CompleteGenoTypes,]
 #p2
 cnjpop.pheno.p2.means.table <- table(cnjpop.pheno.p2.means.df$accession_name) 
 #Complete genotypes should apply for both full and means data
 cnjpop.pheno.p2.CompleteGenoTypes <- names(which(cnjpop.pheno.p2.means.table == 3))
-cnjpop.pheno.p2.df <- cnjpop.pheno.p2.df[cnjpop.pheno.p2.df$accession_name %in% cnjpop.pheno.p2.CompleteGenoTypes,]
 cnjpop.pheno.p2.means.df <- cnjpop.pheno.p2.means.df[cnjpop.pheno.p2.means.df$accession_name %in% cnjpop.pheno.p2.CompleteGenoTypes,]
 
 #Write the means to an output csv file.
