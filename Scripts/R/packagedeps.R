918a4a8c876f4ef4586ba6d6b32cff60d66485c2
Andrew Maule <maule2@wisc.edu>
2021-01-03 16:05:36 -0600

    Updating package dependencies to function in newer framework.

--- Scripts/R/packagedeps.R
+++ Scripts/R/packagedeps.R
@@ -2,24 +2,24 @@
 
 ##### For analyzing phenotypes #####
 install.packages(c("Hmisc"), repos = "http://mirror.las.iastate.edu/CRAN/", dependencies = TRUE)
+install.packages(c("tidyverse"), repos = "http://mirror.las.iastate.edu/CRAN/", dependencies = TRUE)
 install.packages(c("dplyr"), repos = "http://mirror.las.iastate.edu/CRAN/", dependencies = TRUE)
 install.packages(c("pryr"), repos = "http://mirror.las.iastate.edu/CRAN/", dependencies = TRUE)
 install.packages(c("openxlsx","RColorBrewer"), repos = "http://mirror.las.iastate.edu/CRAN/", dependencies = TRUE)
 install.packages(c("lm"), repos = "http://mirror.las.iastate.edu/CRAN/", dependencies = TRUE)
-
-##### For qtl pipeline #####
-#Only install the following packages when running for the first time
+#
+###### For qtl pipeline #####
+##Only install the following packages when running for the first time
 install.packages(c("lme4"), repos = "http://mirror.las.iastate.edu/CRAN/", dependencies = TRUE)
 install.packages(c("qtl"), repos = "http://mirror.las.iastate.edu/CRAN/", dependencies = TRUE)
 install.packages(c("sommer"), repos = "http://mirror.las.iastate.edu/CRAN/", dependencies = TRUE)
 install.packages(c("lattice"), repos = "http://mirror.las.iastate.edu/CRAN/", dependencies = TRUE)
 #IRanges is a part of bioconductor package: ## try http:// if https:// URLs are not supported
-source("http://bioconductor.org/biocLite.R")
-biocLite("IRanges")
-biocLite("GenomicRanges")
-
-install.packages(c("intervals"), repos = "http://mirror.las.iastate.edu/CRAN/", dependencies = TRUE)
-#The following may need to be run as a superuser for the first time on a macosx.  Also, one may manually need to install openmpi.
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager", repos = "http://mirror.las.iastate.edu/CRAN/", dependencies = TRUE)
+BiocManager::install(version = "3.12")
+BiocManager::install(c("IRanges","GenomicRanges"))
+install.packages(c("intervals"), repos = "http://mirror.las.iastate.edu/CRAN/", dependencies = TRUE) #The following may need to be run as a superuser for the first time on a macosx.  Also, one may manually need to install openmpi.
 install.packages(c("Rmpi"), repos = "http://mirror.las.iastate.edu/CRAN/", configure.args="--with-Rmpi-include=/opt/openmpi/include --with-Rmpi-libpath=/opt/openmpi/lib --with-Rmpi-type=OPENMPI", dependencies=TRUE, verbose=TRUE)
 install.packages(c("snow"), repos = "http://mirror.las.iastate.edu/CRAN/", dependencies = TRUE)
 install.packages(c("doSNOW"), repos = "http://mirror.las.iastate.edu/CRAN/", dependencies = TRUE)
@@ -29,6 +29,6 @@ install.packages(c("jsonlite"), repos = "http://mirror.las.iastate.edu/CRAN/", d
 #For generating interesting, interactive plots
 install.packages(c("plotly"), repos = "http://mirror.las.iastate.edu/CRAN/", dependencies = TRUE)
 #For generating correlation-type heatmaps
-install.packages(c("corrplot"), repos = "http://mirror.las.iastate.edu/CRAN/", dependencies = TRUE) install.packages(c("GGally"), repos = "http://mirror.las.iastate.edu/CRAN/", dependencies = TRUE) #Themese package for ggplot2
+install.packages(c("corrplot"), repos = "http://mirror.las.iastate.edu/CRAN/", dependencies = TRUE) 
+install.packages(c("GGally"), repos = "http://mirror.las.iastate.edu/CRAN/", dependencies = TRUE) #Themese package for ggplot2
 install.packages(c("ggthemes"), repos = "http://mirror.las.iastate.edu/CRAN/", dependencies = TRUE)
-
