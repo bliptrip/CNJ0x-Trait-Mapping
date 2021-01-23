c842ca29ffb522e327577cb92351c24cccc21c12
Andrew Maule <maule2@wisc.edu>
2021-01-22 20:12:35 -0600

    Added check to see if all phenotypic values (BLUPs) are zero in the trait cross table, which indicates that no genotypic variance was detected while fitting the model.

--- Scripts/R/qtl_pipeline_04_scanone.R
+++ Scripts/R/qtl_pipeline_04_scanone.R
@@ -23,20 +23,22 @@ source(paste0(workflow,"/configs/model.cfg"))
 source('./usefulFunctions.R')
 
 scanoneCB <- function(trait.cfg, trait.path, funArgs) {
-    cross            <- readRDS(file=paste0(trait.path,"/cross.rds"))
-    scan.one.perms   <- readRDS(paste0(trait.path, "/operms.rds"))
+    cross <- readRDS(file=paste0(trait.path,"/cross.rds"))
     trait <- trait.cfg$trait
-    print(paste0("Running scanone() with model: ",trait.cfg$model, " | trait: ", trait))
-    trait_subsubfolder_fpath = paste0(trait.path, '/', trait) 
-    dir.create(trait_subsubfolder_fpath, showWarnings = FALSE)
-    scan.one         <- scanone(cross, pheno.col=trait, method=qtl_method, verbose=FALSE)
-    scan.one.sum <- summary(scan.one, perms=scan.one.perms[,trait], alpha=scanone_alpha, pvalues=TRUE)
-    #Now perform a refineqtl() to make the scanone object compatible with stepwiseqtl() results
-    scan.one.qtl <- refineqtl(cross, chr=scan.one.sum$chr, pos=scan.one.sum$pos, keeplodprofile=TRUE, method=qtl_method)
-    attr(scan.one.qtl, "formula") <-  paste0("y ~ ",paste0(scan.one.qtl$altname,collapse=" + "))
-    saveRDS(scan.one, file=paste0(trait_subsubfolder_fpath, "/scanone.rds"), compress=TRUE)
-    saveRDS(scan.one.sum, file=paste0(trait_subsubfolder_fpath, "/scanone.summary.rds"), compress=TRUE)
-    saveRDS(scan.one.qtl, file=paste0(trait_subsubfolder_fpath, "/scanone.qtl.rds"), compress=TRUE)
+    if( any(cross$pheno[trait] != 0) ) {
+        scan.one.perms   <- readRDS(paste0(trait.path, "/operms.rds"))
+        print(paste0("Running scanone() with model: ",trait.cfg$model, " | trait: ", trait))
+        trait_subsubfolder_fpath = paste0(trait.path, '/', trait) 
+        dir.create(trait_subsubfolder_fpath, showWarnings = FALSE)
+        scan.one         <- scanone(cross, pheno.col=trait, method=qtl_method, verbose=FALSE)
+        scan.one.sum <- summary(scan.one, perms=scan.one.perms[,trait], alpha=scanone_alpha, pvalues=TRUE)
+        #Now perform a refineqtl() to make the scanone object compatible with stepwiseqtl() results
+        scan.one.qtl <- refineqtl(cross, chr=scan.one.sum$chr, pos=scan.one.sum$pos, keeplodprofile=TRUE, method=qtl_method)
+        attr(scan.one.qtl, "formula") <-  paste0("y ~ ",paste0(scan.one.qtl$altname,collapse=" + "))
+        saveRDS(scan.one,     file=paste0(trait_subsubfolder_fpath, "/scanone.rds"), compress=TRUE)
+        saveRDS(scan.one.sum, file=paste0(trait_subsubfolder_fpath, "/scanone.summary.rds"), compress=TRUE)
+        saveRDS(scan.one.qtl, file=paste0(trait_subsubfolder_fpath, "/scanone.qtl.rds"), compress=TRUE)
+    }
 }
 
 loopThruTraits(workflow, scanoneCB)
