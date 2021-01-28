99ebc6e0ecf5b453220fa50043f5ab76f818f1be
Andrew Maule <maule2@wisc.edu>
2021-01-28 09:48:26 -0600

    Added check that not all BLUPs are zero-valued.

--- Scripts/R/qtl_pipeline_07_stepwiseqtl.local.R
+++ Scripts/R/qtl_pipeline_07_stepwiseqtl.local.R
@@ -22,14 +22,16 @@ source('./usefulFunctions.R')
 #Loop over all mmers, trait groups, subgroups, and perform stepwiseqtl()
 stepwiseQtlCB <- function(trait.cfg, trait.path, funArgs) {
     cross <- readRDS(file=paste0(trait.path,"/cross.rds"))
-    scan.two.perms <- readRDS(paste0(trait.path, "/operms.2D.rds"))
-    pens  <- calc.penalties(scan.two.perms)
     trait <- trait.cfg$trait
-    print(paste0("Running stepwiseqtl() with model: ",trait.cfg$model, " | trait: ", trait))
-    trait_subsubfolder_fpath = paste0(trait.path, '/', trait) 
-    dir.create(trait_subsubfolder_fpath, showWarnings = FALSE)
-    scan.sw <- stepwiseqtl(cross,pheno.col=trait,max.qtl=qtl_max,method=qtl_method,penalties=pens,additive.only=FALSE, keeplodprofile=TRUE, keeptrace=TRUE)
-    saveRDS(scan.sw, file=paste0(trait_subsubfolder_fpath, "/scansw.rds"), compress=TRUE)
+    if( any(cross$pheno[trait] != 0) ) {
+		scan.two.perms <- readRDS(paste0(trait.path, "/operms.2D.rds"))
+		pens  <- calc.penalties(scan.two.perms)
+		print(paste0("Running stepwiseqtl() with model: ",trait.cfg$model, " | trait: ", trait))
+		trait_subsubfolder_fpath = paste0(trait.path, '/', trait) 
+		dir.create(trait_subsubfolder_fpath, showWarnings = FALSE)
+		scan.sw <- stepwiseqtl(cross,pheno.col=trait,max.qtl=qtl_max,method=qtl_method,penalties=pens,additive.only=FALSE, keeplodprofile=TRUE, keeptrace=TRUE)
+		saveRDS(scan.sw, file=paste0(trait_subsubfolder_fpath, "/scansw.rds"), compress=TRUE)
+	}
 }
 
 #Loop through all legitimate traits and build collated qtl file.
