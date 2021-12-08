1c094dcef0aaf30c30ba9ad5ee9cd1777d3da2b1
Andrew Maule <developer@bliptrip.net>
2021-12-07 21:45:57 -0700

    More robust error checking.

--- Scripts/R/qtl_pipeline_07_stepwiseqtl.local.R
+++ Scripts/R/qtl_pipeline_07_stepwiseqtl.local.R
@@ -25,14 +25,42 @@ stepwiseQtlCB <- function(trait.cfg, trait.path, funArgs) {
     cross <- readRDS(file=paste0(trait.path,"/cross.rds"))
     trait <- trait.cfg$trait
     if( any(cross$pheno[trait] != 0) ) {
-		scan.two.perms <- readRDS(paste0(trait.path, "/operms.2D.rds"))
-		pens  <- calc.penalties(scan.two.perms)
-		print(paste0("Running stepwiseqtl() with model: ",trait.cfg$model, " | trait: ", trait))
-		trait_subsubfolder_fpath = paste0(trait.path, '/', trait) 
-		dir.create(trait_subsubfolder_fpath, showWarnings = FALSE)
-		scan.sw <- stepwiseqtl(cross,pheno.col=trait,max.qtl=qtl_max,method=qtl_method,penalties=pens,additive.only=FALSE, keeplodprofile=TRUE, keeptrace=TRUE)
-		saveRDS(scan.sw, file=paste0(trait_subsubfolder_fpath, "/scansw.rds"), compress=TRUE)
-	}
+        scan.two.perms <- readRDS(paste0(trait.path, "/operms.2D.rds"))
+        pens  <- calc.penalties(scan.two.perms)
+        print(paste0("Running stepwiseqtl() with model: ",trait.cfg$model, " | trait: ", trait))
+        trait_subsubfolder_fpath = paste0(trait.path, '/', trait) 
+        dir.create(trait_subsubfolder_fpath, showWarnings = FALSE)
+        tryCatch({
+                sink(paste0(trait_subsubfolder_fpath,"/scansw.int.trace"))
+                scan.sw <- stepwiseqtl(cross,pheno.col=trait,max.qtl=qtl_max,method=qtl_method,penalties=pens,additive.only=FALSE, keeplodprofile=TRUE, keeptrace=TRUE, verbose=TRUE)
+                pLOD <- attr(scan.sw, "pLOD")
+                if( !is.infinite(pLOD) && !is.nan(pLOD) ) {
+                    saveRDS(scan.sw, file=paste0(trait_subsubfolder_fpath, "/scansw.rds"), compress=TRUE)
+                }
+            },
+            warning = function(w)
+            {
+                sink()
+                cat(paste0("stepwiseqtl() threw warning: ",w))
+                sink(paste0(trait_subsubfolder_fpath,"/scansw.add.trace"))
+                scan.sw <- stepwiseqtl(cross,pheno.col=trait,max.qtl=qtl_max,method=qtl_method,penalties=pens,additive.only=TRUE, keeplodprofile=TRUE, keeptrace=TRUE, verbose=TRUE)
+                pLOD <- attr(scan.sw, "pLOD")
+                if( !is.infinite(pLOD) && !is.nan(pLOD) ) {
+                    saveRDS(scan.sw, file=paste0(trait_subsubfolder_fpath, "/scansw.rds"), compress=TRUE)
+                }
+                return(NULL)
+            },
+            error = function(e)
+            {
+                cat(paste0("stepwiseqtl() threw error: ",e))
+                browser()
+                return(NULL)
+            },
+            finally = {
+                sink()
+            }
+        )
+    }
 }
 
 #Loop through all legitimate traits and build collated qtl file.
