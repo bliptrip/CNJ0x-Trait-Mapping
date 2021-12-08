d7375ffa60c4e4f86adb632eeb8d6f997d252746
Andrew Maule <developer@bliptrip.net>
2021-12-07 22:01:47 -0700

    Adding code to generate BLUP-Raw trait value correlations.

---
+++ Scripts/R/qtl_pipeline_01_05_genBLUPCorrPlots.R
@@ -0,0 +1,24 @@
+#!/usr/bin/env RScript
+
+#This is a QTL pipeline script given to me by Luis, but I've adapted to work with the Vorsa upright datasets from 2011-2014
+#
+#NOTE: This particular script generates the correlation plots derived from means of two Vorsa populations along with the correlations of 
+# both populations combined..
+
+# loading libraries
+source('./usefulFunctions.R')
+library(tidyverse)
+
+workflow <- get0("workflow", ifnotfound="../../Workflows/1")
+
+#In case we override the workflow on the command-line
+args = commandArgs(trailingOnly=TRUE)
+if(length(args)!=0) {
+    for(i in 1:length(args)){
+        eval(parse(text=args[[i]]))
+    }
+}
+
+blups.collated.wide.tb <- readRDS(paste0(workflow,"/traits/blups_collated.wide.rds"))
+blup_raw_cors <- blups.collated.wide.tb %>% group_by(model,trait) %>% select(blup,raw) %>% summarize(brcorr=cor(blup,raw,use="pairwise.complete.obs",method="pearson"))
+write_csv(blup_raw_cors, file=paste0(workflow,"/traits/blups_raw_corrs.csv"))
