5afcf4542440c57356c61ce072ee1f4b70ab7509
Andrew Maule <developer@bliptrip.net>
2023-07-26 00:49:58 -0500

    Adding code to calculate mean effect sizes of cliques -- Used to search for QTL that are consistent across 3 of 4 models.

--- Scripts/Python/generate_consistentQTL.py
+++ Scripts/Python/generate_consistentQTL.py
@@ -8,7 +8,8 @@ import numpy as np
 import re
 from datar import f
 from datar.all import arrange, c, colnames, ungroup, rename, bind_rows
-from datar.base import is_in
+from datar.base import is_in, all_
+from datar.base import any_
 from datar.core.tibble import Tibble
 from datar.dplyr import mutate, filter, inner_join, select, group_by, group_modify
 from datar.tidyr.nest import nest,unnest
@@ -26,6 +27,12 @@ from datar.core.broadcast import broadcast_to
 from datar.core.operator import _binop
 
 from scipy.interpolate import interp1d
+
+columns_highlighted = ("method", "trait", "chr", "model", "position", "nearest_marker", "qtl_lod", "marker_variance", "model_variance", "interval_left", "interval_right", "population","study")
+columns_highlighted_i = tuple(["id"] + list(columns_highlighted))
+columns_effects_extended = ("effect_mean_AD","effect_mean_AC","effect_mean_BC","effect_mean_BD","effect_se_AD","effect_se_AC","effect_se_BC","effect_se_BD","AvB","CvD","Int")
+columns_output = ("id", "trait", "model", "chr", "position", "mean_marker_variance", "mean_model_variance", "mean_combined_variance", "population", "method", "study", "interval_left", "interval_right", "count", "model_count", "study_count", "trait_count", "method_count", "population_count")
+columns_output_extended = tuple(["mean_" + o for o in columns_effects_extended])
 
 @register_verb(types=Tibble,context=Context.EVAL)
 def group_trait(data,**kwargs):
@@ -78,14 +85,26 @@ def merge_qtl(df):
         intervals_right= [goverlap.nodes[i]["interval_right"] for i in clique]
         merger = dict((k,set([])) for k in goverlap.nodes[list(clique)[0]].keys())
         merger["marker_variance"] = [] #Marker variance needs to be a list and not a set, in case of repeated variance values
+        merger["model_variance"] = [] #Model variance needs to be a list and not a set, in case of repeated variance values
+        merger["combined_variance"] = []
         merger["interval_left"] = np.max(intervals_left)
         merger["interval_right"] = np.min(intervals_right)
         merger['position'] = (merger['interval_left'] + merger['interval_right'])/2
+        if all_(is_in(columns_effects_extended,list(merger.keys()))):
+            for k in columns_effects_extended:
+                merger[k] = []
         for n in [goverlap.nodes[i] for i in clique]:
             n = dict([(k,set([v])) for k,v in n.items()]) #Convert to set for merger
             for k in n.keys():
                 if( k == "marker_variance" ):
                     merger[k].append(list(n[k])[0])
+                elif( k == "model_variance" ):
+                    if not np.isnan(list(n[k])[0]):
+                        merger[k].append(list(n[k])[0])
+                        merger["combined_variance"].append((float(list(n["marker_variance"])[0]) * float(list(n[k])[0]))/100) #divide by 100 b/c each factor is a percent
+                elif (k in columns_effects_extended):
+                    if not np.isnan(list(n[k])[0]):
+                        merger[k].append(list(n[k])[0])
                 elif( k not in ['interval_left', 'interval_right', 'position'] ):
                     merger[k] = merger[k] | n[k] #Intersect sets
         #Convert all sets to string-concatenated lists
@@ -94,16 +113,26 @@ def merge_qtl(df):
                 merger[k] = '+'.join([str(e) for e in merger[k]])
         merger['count'] = len(clique)
         merger['mean_marker_variance'] = (eval(merger['marker_variance']))/merger['count']
+        if merger['model_variance'] != "":
+            merger['mean_model_variance'] = (eval(merger['model_variance']))/merger['count']
+            merger['mean_combined_variance'] = (eval(merger['combined_variance']))/merger['count']
+        else:
+            merger['mean_model_variance'] = np.nan
+            merger['mean_combined_variance'] = np.nan
         merger['model_count'] = len(merger['model'].split('+'))
         merger['study_count'] = len(merger['study'].split('+'))
         merger['method_count'] = len(merger['method'].split('+'))
         merger['population_count'] = len(merger['population'].split('+'))
         merger['trait_count'] = len(merger['trait'].split('+'))
+        if all_(is_in(columns_effects_extended,list(merger.keys()))):
+            for k in columns_effects_extended:
+                try:
+                    merger['mean_'+k] = (eval(merger[k])/merger['count'])
+                except SyntaxError as se:
+                    print("Invalid syntax for key {}: \"{}\".  Merger: \"{}\"".format(k,merger[k],merger))
         nstack.append(merger)
     nstack = tibble(pd.DataFrame(nstack))
     return(nstack)
-
-columns_output = ("id", "trait", "model", "chr", "position", "mean_marker_variance", "population", "method", "study", "interval_left", "interval_right", "count", "model_count", "study_count", "trait_count", "method_count", "population_count")
 
 @register_verb(types=Tibble,context=Context.EVAL)
 def filter_qtl(_data, *filters):
@@ -127,7 +156,11 @@ def filter_qtl(_data, *filters):
         if isinstance(condition, Series):
             condition = condition.values
         _data = ungroup(_data, __ast_fallback="normal")[condition]
-    _data = _data[list(columns_output)]
+    if all_(is_in(columns_output_extended,list(_data.keys()))):
+        co = list(columns_output) + list(columns_output_extended)
+    else:
+        co = list(columns_output)
+    _data = _data[list(co)]
     _data.sort_values(by="mean_marker_variance", ascending=False, inplace=True, ignore_index=True)
     return(_data)
 
@@ -202,7 +235,7 @@ ssr2c.setSSRMapPath("../../Data/genetic_data/RawData/schlautman2015_ssr_map.csv"
 ssr2c.setCompositeMapPath("../../Data/genetic_data/RawData/consensusMapAll2.csv")
 ssr2c.genMap()
 
-#Read in model config file that maps trait names to 
+#Read in model config file that maps trait names to abbreviated labels
 model_config = tibble(pd.read_csv("../../Data/publication/tables/model-traits.cfg.csv"))
 model_traits = pd.Index(model_config.trait).factorize()
 model_short = pd.Index(model_config.label_short).factorize()
@@ -211,22 +244,14 @@ interval_max = 15 #Eliminates wide QTL whose confidence interval is too large
 mvariance_min = 10 #Eliminates QTL with smaller marker variances
 
 
-columns_highlighted = ("method", "trait", "chr", "model", "position", "nearest_marker", "qtl_lod", "marker_variance", "interval_left", "interval_right", "population","study")
-#filter_small = (f.count >= 2) & (f.mean_marker_variance > 10.0) & (f.model_count >= 3)
-
 #My mapping CNJ02
 cnj02_groups=[["MFM","UMFM","UTBM","UBW","UBM"],["TY","SFY","PAC"],["UBL","UKLvW","UKEC","ULvW"],["UL","UDM"],["UNP","UNAF"]] #Group QTL by traits that have similar meaning/correlation
-qtls_cnj02 = tibble(pd.read_csv("../../Workflows/9/traits/qtl_collated.consensus.csv")) >> \
+qtls_cnj02 = tibble(pd.read_csv("../../Workflows/9/traits/effects_collated.wide.csv")) >> \
                 filter((f.chr2 == '') | np.isnan(f.chr2)) >> \
-                rename(qtl_lod="qtl.lod",
-                    marker_variance="marker.variance",
-                    nearest_marker="nearest.marker",
-                    interval_left="interval.left",
-                    interval_right="interval.right") >> \
                 filter((f.interval <= interval_max) & (f.marker_variance >= mvariance_min)) >> \
                 mutate(population="cnj02") >> \
-                mutate(study="self") >> \
-                select(columns_highlighted) >> \
+                mutate(study="self") >>  \
+                select(columns_highlighted+columns_effects_extended) >> \
                 long2short(trait='trait',l2smap=model_trait_map)
 
 #nis = np.min(qtls_cnj02.index) #next index start
@@ -267,6 +292,16 @@ qtls_m = qtls_cnj02 >> \
 
 qtls_m.to_csv("../../Data/publication/tables/cnj02_qtl_collated.consensus.csv", index=False)
 
+qtls_nay = qtls_cnj02 >> \
+            filter(f.model != 'all-years') >> \
+            group_trait(property_trait='trait', name='group_category', groups=[]) >> \
+            gropby(f.group_category,f.chr) >> \
+            group_modify(merge_qtl) >> \
+            ungroup() >> \
+            filter_qtl()
+
+qtls_nay.to_csv("../../Data/publication/tables/cnj02_qtl_collated.consensus.nay.csv", index=False)
+
 qtls_g = qtls_cnj02 >> \
             group_trait(property_trait='trait', name='group_category', groups=cnj02_groups) >> \
             group_by(f.group_category,f.chr) >> \
@@ -281,17 +316,12 @@ qtls_g.to_csv("../../Data/publication/tables/cnj02_qtl_collated.grouped.consensu
 cnj04_groups=[["UNP","UNAF"],["USL","UDM"],["UNS","UBW","MFM","UBM""UMFM"],["TY","SFY","UNB","UTBM"],["UBL","UKLvW","UKEC","ULvW"]] #Group QTL by traits that have similar meaning/correlation
 
 
-qtls_cnj04 = tibble(pd.read_csv("../../Workflows/10/traits/qtl_collated.consensus.csv")) >> \
+qtls_cnj04 = tibble(pd.read_csv("../../Workflows/10/traits/effects_collated.wide.csv")) >> \
                 filter((f.chr2 == '') | np.isnan(f.chr2)) >> \
-                rename(qtl_lod="qtl.lod",
-                    marker_variance="marker.variance",
-                    nearest_marker="nearest.marker",
-                    interval_left="interval.left",
-                    interval_right="interval.right") >> \
                 filter((f.interval <= interval_max) & (f.marker_variance >= mvariance_min)) >> \
                 mutate(population="cnj04") >> \
                 mutate(study="self") >> \
-                select(columns_highlighted) >> \
+                select(columns_highlighted+columns_effects_extended) >> \
                 long2short(trait='trait',l2smap=model_trait_map)
 
 nis = nie #Previous value
@@ -331,6 +361,17 @@ qtls_m = qtls_cnj04 >> \
 
 qtls_m.to_csv("../../Data/publication/tables/cnj04_qtl_collated.consensus.csv", index=False)
 
+qtls_nay = qtls_cnj04 >> \
+            filter(f.model != 'all-years') >> \
+            group_trait(property_trait='trait', name='group_category', groups=[]) >> \
+            group_by(f.group_category,f.chr) >> \
+            group_modify(merge_qtl) >> \
+            ungroup() >> \
+            filter_qtl()
+#            filter_qtl(f.count >= 2, f.mean_marker_variance > 5)
+
+qtls_nay.to_csv("../../Data/publication/tables/cnj04_qtl_collated.consensus.nay.csv", index=False)
+
 qtls_g   = qtls_cnj04 >> \
                 group_trait(property_trait='trait', name='group_category', groups=cnj04_groups) >> \
                 group_by(f.group_category,f.chr) >> \
@@ -361,6 +402,7 @@ qtls = tibble(pd.read_csv("../../Data/publication/tables/diazGarcia2018MassivePh
 qtls_massive = qtls >> \
                     mutate(interval_left=f.position-(f.lod_15/2),
                            interval_right=f.position+(f.lod_15/2),
+                           model_variance=np.nan,
                            qtl_lod=np.nan) >> \
                     filter((f.lod_15 <= interval_max) & (f.marker_variance >= mvariance_min)) >> \
                     mutate(study="diazGarcia2018_massive") >> \
@@ -392,6 +434,7 @@ qtls = tibble(pd.read_csv("../../Data/publication/tables/diazGarcia2018ImagePhen
 qtls_image = qtls >> \
                 mutate(interval_left=f.position-(f.lod_15/2),
                        interval_right=f.position+(f.lod_15/2),
+                       model_variance=np.nan,
                        qtl_lod=np.nan,
                        population="gryg",
                        method="stepwiseqtl") >> \
@@ -427,6 +470,7 @@ qtls_schlautman = tibble(pd.read_csv("../../Data/publication/tables/schlautman20
                qtl_lod=f.lod_2) >> \
         filter(((f.interval_right - f.interval_left) <= interval_max) & (f.marker_variance >= mvariance_min)) >> \
         mutate(population="cnj02",
+                model_variance=np.nan,
                method="CIM") >> \
         mutate(study="schlautman") >> \
         convert_ssr2composite(ssr2c_class=ssr2c) >> \
@@ -442,6 +486,7 @@ qtls_schlautman.to_csv("../../Data/publication/tables/schlautman2015_cnj0x.raw.c
 qtls_merged = qtls_schlautman >> \
                 bind_rows(qtls_cnj02) >> \
                 bind_rows(qtls_cnj04) >> \
+                select(columns_highlighted_i) >> \
                 group_trait(property_trait='trait', name='group_category', groups=schlautman_groups) >> \
                 mutate(count = 1) >> \
                 group_by(f.group_category,f.chr) >> \
@@ -460,6 +505,7 @@ cnj0x_massive_groups=[["MFM","UMFM","UTBM","UBW","UBM","UBL"],["TY","SFY","PAC"]
 qtls_merged = qtls_massive >> \
                 bind_rows(qtls_cnj02) >> \
                 bind_rows(qtls_cnj04) >> \
+                select(columns_highlighted_i) >> \
                 group_trait(property_trait='trait', name='group_category', groups=cnj0x_massive_groups) >> \
                 mutate(count = 1) >> \
                 group_by(f.group_category,f.chr) >> \
@@ -476,6 +522,7 @@ cnj0x_image_groups=[["MFM","UMFM","UTBM","UBW","UBM","UBL","BL","BW","BA"],["TY"
 qtls_merged = qtls_image >> \
                 bind_rows(qtls_cnj02) >> \
                 bind_rows(qtls_cnj04) >> \
+                select(columns_highlighted_i) >> \
                 group_trait(property_trait='trait', name='group_category', groups=cnj0x_image_groups) >> \
                 mutate(count = 1) >> \
                 group_by(f.group_category,f.chr) >> \
@@ -489,9 +536,10 @@ qtls_merged.to_csv("../../Data/publication/tables/diazGarciaImagePhenotyping2018
 #Merge Schlautman 2015 qtl results and Diaz-Garcia ImagePhenotyping results with CNJ0x results
 
 qtls_merged = qtls_schlautman >> \
-                bind_rows(qtls_image) >> \
                 bind_rows(qtls_cnj02) >> \
                 bind_rows(qtls_cnj04) >> \
+                bind_rows(qtls_image) >> \
+                select(columns_highlighted_i) >> \
                 group_trait(property_trait='trait', name='group_category', groups=cnj0x_image_groups) >> \
                 mutate(count = 1) >> \
                 group_by(f.group_category,f.chr) >> \
@@ -504,9 +552,9 @@ qtls_merged.to_csv("../../Data/publication/tables/schlautman2015_diazGarciaImage
 
 #Dump all ungrouped QTL to a csv file so that IDs can be traced
 qtls_raw = qtls_schlautman >> \
-                bind_rows(qtls_image) >> \
-                bind_rows(qtls_massive) >> \
                 bind_rows(qtls_cnj02) >> \
-                bind_rows(qtls_cnj04)
+                bind_rows(qtls_cnj04) >> \
+                bind_rows(qtls_image) >> \
+                bind_rows(qtls_massive)
 
 qtls_raw.to_csv("../../Data/publication/tables/all.raw.consensus.csv", index=False)
